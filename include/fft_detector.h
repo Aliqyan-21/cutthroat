452ef35e74912c33e7a9edee6a8d1f9c04e4a4c7
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-03-13 17:45:55 +0530

    feat(FFTDetector): calc_aaps(...) implementation

--- include/fft_detector.h
+++ include/fft_detector.h
@@ -9,15 +9,18 @@ public:
   FFTDetector(ImgData &id);
   void run_detection();
   void make_fft_ppm(const std::string &outfile_path);
+  void calculate_aaps(const std::string &outfile_path = "");
 
   std::vector<cmplx> get_spectrum_1d();
   std::vector<cmplx> get_spectrum_2d();
+  std::vector<float> get_aaps();
 
 private:
   ImgData &img_;
   std::vector<float> gray_;
   std::vector<cmplx> spectrum_1d_;
   std::vector<cmplx> spectrum_2d_;
+  std::vector<float> aaps_;
 
   void calculate_fft_1d();
   void calculate_fft_2d();
